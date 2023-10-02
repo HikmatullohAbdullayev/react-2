588ab828f638632b64bf8f26e03ea6bd3627bdc2
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-10-02 11:26:43 +0500

    nmadur

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import './App.css'
-import { Test_components } from './components/Test-component'
+// import { Test_components } from './components/Test-component'
+// const obj = {
+//   name: "allakim",
+//   lastName:"falonchi",
+//   age:  23
+
+// }
+// const click = () =>{
+//   console.log("nma gap ");
+// }
+
 function App() {
 
   return (
     <>
       <div>
-        <h1>jdjksj</h1>
+        {/* <h1>jdjksj</h1>
         <p>kdkjs</p>
       <Test_components/>
-      <Test_components/>
-      <Test_components/>
-      <Test_components/>
-      <Test_components/>
+      <h1 onClick={click}>doctor name: {obj.name}</h1>
+      <h1 className='testClass'>{obj.lastName}</h1>
+      <h1>{obj.age}</h1> */}
+
+      
+
+
+
+
       </div>
      
     </>
